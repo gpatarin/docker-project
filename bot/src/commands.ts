8b252aed602e80dd933470fb31b1e8c906fbcb3f
Houle Adrien <adrienhoule01@gmail.com>
2023-04-21 08:22:18 +0200

    added working bot

--- bot/src/commands.ts
+++
@@ -1,6 +0,0 @@
-import { CommandInteraction, Client } from "discord.js";
-import { Command } from "./command";
-import { Poll } from "./poll";
-import { Hello } from "./hello";
-
-export const Commands: Command[] = [Poll, Hello];
--- bot/src/index.ts
+++ bot/src/index.ts
@@ -1,16 +1,17 @@
 import express from 'express';
-import { CommandInteraction, ChatInputApplicationCommandData, Client, ClientOptions } from "discord.js";
-import interactionCreate from "./listeners/interactionCreate";
-import ready from "./listeners/ready";
-
 import * as dotenv from 'dotenv'
+import { Client, IntentsBitField } from 'discord.js';
+import { onInteraction } from './events/OnInteraction';
+import { onReady } from './events/OnReady';
 dotenv.config()
 
 const API_KEY = process.env['DISCORD_TOKEN'];
 const PORT = process.env['PORT'];
+const GUILD_ID = process.env['GUILD_ID'];
 
 if (!API_KEY) throw Error('DISCORD_TOKEN not existing. You forgot to set it as env variable.')
 if (!PORT) throw Error('PORT not existing. You forgot to set it as env variable.')
+if (!GUILD_ID) throw Error('GUILD_ID not existing. You forgot to set it as env variable.')
 
 const server = express();
 
@@ -22,15 +23,17 @@ server.listen(PORT, () => {
 console.log(`Listening on :${PORT}`)
 })
 
-const client = new Client({
-intents: []
-});
-
-ready(client);
-interactionCreate(client);
-
-client.login(API_KEY);
-
-console.log(client);
 
 
+const BOT = new Client({
+    intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMessageReactions]
+    });
+    
+BOT.on("ready", async () => await onReady(BOT));
+
+BOT.on(
+    "interactionCreate",
+    async (interaction) => await onInteraction(interaction)
+);
+
+BOT.login(process.env.BOT_TOKEN);
