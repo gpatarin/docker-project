c9b5c4a48db65b184ee6e31543d196c1f984d739
gpatarin <gael.patarin@outlook.com>
2023-04-21 23:47:34 +0200

    Incroyable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import background from '../public/bg.jpg';
 
 import { fetchPolls } from "./api/poll/poll"
 
@@ -14,25 +15,54 @@ interface Poll {
 
 function App() {
   const [data, setData] = useState<Poll[]>([]);
+  const [play, setPlay] = useState(false);
+  const [audio] = useState(new Audio('https://www.myinstants.com/media/sounds/mlg-horns-sound-effect.mp3'))
+
+  function mlg() {
+    setPlay(() => {
+      audio.play();
+      return true;
+    })
+  }
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlay(false));
+  }, [])
 
   useEffect(() => {
     fetchPolls().then((res) => {
       console.log(res);
-      setData(res);
+      setData(res.reverse());
     });
   }, []);
 
   return (
-    <div className="flex flex-col" >
-      {data.map((poll) => (
-        <div className="flex flex-col items-center bg-white shadow-md rounded-md m-2 p-5">
-          <span className="text-xl font-bold">{poll.question}</span>
-          <div className="w-full flex justify-around text-lg">
-            <div>👍 {poll.agree}</div>
-            <div>👎 {poll.disagree}</div>
+    <div className="flex flex-col items-center" >
+      {data.map((poll) => {
+        const total = poll.agree + poll.disagree
+        const ratioAgree = (poll.agree / total);
+        const ratioDisagree = (poll.disagree / total);
+
+        return (
+          <div onClick={mlg} className="flex flex-col items-center bg-white shadow-md rounded-md m-2 p-5 hover:animate-bounce">
+            <span className="text-xl font-bold">{poll.question}</span>
+            <div className="w-full flex text-lg">
+              {ratioAgree !== 0 && (<div
+                className={`w-full bg-green-900 hover:animate-spin`}
+                style={{ flex: isNaN(ratioAgree) ? '0.5' : ratioAgree.toString() }}
+              >
+                👍 {poll.agree}
+              </div>)}
+              {ratioDisagree !== 0 && (<div
+                className={`w-full bg-red-900 hover:animate-spin`}
+                style={{ flex: isNaN(ratioDisagree) ? '0.5' : ratioDisagree.toString() }}
+              >
+                👎 {poll.disagree}
+              </div>)}
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   )
 }
